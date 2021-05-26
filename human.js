57a14ea230352baf4e6b2211fdecc0785045641d
Kevin Wilburn <wilburnkevin200@gmail.com>
2021-05-26 18:17:17 -0400

    modified Human class to HumanOne and created HumanTwo class

--- human.js
+++ human.js
@@ -1,6 +1,7 @@
 const{ Player }= require("./player");
+const Prompt = ("prompt-sync");
 
-class Human extends Player{
+class HumanOne extends Player{
     constructor(){
         super();
     }
@@ -22,7 +23,27 @@ class Human extends Player{
 }
 
 
+class HumanTwo extends Player{
+    constructor(){
+        super();
+    }
+    chooseOption(){
+        this.gesture= prompt("Choose a gesture " + this.stringBuilder(this.arrayGestures))//make sure using prompt-sync
+    }
+    
+    stringBuilder(arrayGestures){
+        let menuString ="";
+        for(let i = 0; i <arrayGestures; i++){
+            this.arrayGestures.push(menuString);
+        }
 
 
+    }
+
+}
+
+
+module.exports.HumanOne = HumanOne;
+module.exports.HumanTwo = HumanTwo;
 
 
